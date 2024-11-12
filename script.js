1c23b523b40a3bfbde311d2e71f9a46c74666feb
FabienMo <fabien.moline@gmail.com>
2024-11-12 22:54:06 +0100

    img

--- script.js
+++ script.js
@@ -180,23 +180,23 @@ document.addEventListener("mousemove", function (event) {
 
 // FORBIDEN INSPECT
 
-// document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 
-// function ctrlShiftKey(e, keyCode) {
-//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-// }
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
 
-// document.onkeydown = (e) => {
-//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-//   if (
-//     event.keyCode === 123 ||
-//     ctrlShiftKey(e, "I") ||
-//     ctrlShiftKey(e, "J") ||
-//     ctrlShiftKey(e, "C") ||
-//     (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-//   )
-//     return false;
-// };
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, "I") ||
+    ctrlShiftKey(e, "J") ||
+    ctrlShiftKey(e, "C") ||
+    (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+  )
+    return false;
+};
 
 // PORTFOLIO MODAL
 const modal = document.querySelector(".modal");
